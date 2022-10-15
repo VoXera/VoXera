2102f2b93837ade0f0d65437f0232ff98b397102
Iman <imankhanijazani@gmail.com>
2022-10-15 19:55:11 +0330

    make folder for PerDeepKE and moving

---
+++ src/VoXera/Keyword_Extraction/PerDeepKE/VXperdeepke.py
@@ -0,0 +1,16 @@
+class KeywordExtraction():
+    def __init__(self):
+        pass
+    
+    def load_model(self):
+        self.normalizer = Normalizer()
+        self.tokenizer = Tokenizer()
+        self.embeder = Embedding()
+    
+    def preprocess():
+        pass
+    def infer(self, text):
+        pass
+
+    def postprocess():
+        pass
